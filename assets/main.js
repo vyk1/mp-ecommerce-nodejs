8014e1238020e88edf3f992ac3c1526ef78197df
Victoria <victoriabotelho14@gmail.com>
2022-01-31 00:54:52 -0300

    feat: adds mp dependencies f+b

---
+++ assets/main.js
@@ -0,0 +1,56 @@
+const mp = new MercadoPago('APP_USR-6096a634-0b35-452c-94c9-a18adb8ffb15', {
+    locale: 'pt-BR'
+});
+
+function createCheckoutButton(preferenceId) {
+
+    mp.checkout({
+        preference: {
+            id: preferenceId
+        },
+        render: {
+            container: '#button-checkout',
+            label: 'Pague a compra',
+        }
+    });
+}
+
+function checkout(el) {
+
+    const orderData = {
+        img: el.getAttribute('data-img'),
+        title: el.getAttribute('data-title'),
+        price: el.getAttribute('data-price')
+    }
+
+    el.hidden = true
+
+    fetch("/create-preference", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (preference) {
+            createCheckoutButton(preference.id);
+
+            $(".shopping-cart").fadeOut(500);
+            setTimeout(() => {
+                $(".container_payment").show(500).fadeIn();
+            }, 500);
+        })
+        .catch(function (e) {
+            console.log(e);
+            alert("Unexpected error");
+            $('#checkout-btn').attr("disabled", false);
+        })
+        .finally(function () {
+            el.setAttribute('disabled', true)
+        })
+
+}
+
